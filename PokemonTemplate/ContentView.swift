9d33396bd65fe88eceac1ac51b9e952cc270b224
Mark Kinoshita <mkinoshita22@gmail.com>
2024-08-13 19:47:46 -0700

    Initial Commit

---
+++ PokemonTemplate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonTemplate
+//
+//  Created by Mark Kinoshita on 8/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonTemplate/PokemonTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTemplateApp.swift
+//  PokemonTemplate
+//
+//  Created by Mark Kinoshita on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
